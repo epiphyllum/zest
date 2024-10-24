301e05eba0e48406a91bac13283083679e6f0be4
Your Name <you@example.com>
2024-10-24 09:00:07 +0800

    updated

--- renren-admin/renren-admin-server/src/main/java/io/renren/zmanager/JMerchantManager.java
+++ renren-admin/renren-admin-server/src/main/java/io/renren/zmanager/JMerchantManager.java
@@ -222,7 +222,7 @@ public class JMerchantManager {
 
     public void changeState(JMerchantEntity entity, String newState, String cusid) {
         String oldState = entity.getState();
-        log.info("{} -> {}", oldState, newState);
+        System.out.println(oldState + "->"  + newState);
 
         // 状态没有变化
         if (oldState.equals(newState)) {
