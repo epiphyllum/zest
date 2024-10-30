a0e3be091d5cc0a69b59ff0df0a85740781eb920
Your Name <you@example.com>
2024-10-30 11:39:23 +0000

    updated

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export nacos-host=127.0.0.1
+export redis-host=127.0.0.1
+
 nohup java -server -Xms4096M -Xmx4096M -jar renren-admin/renren-admin-server/target/renren-admin-server.jar --spring.profiles.active=prod --server.port=8082 >> zest.log &
