c67bde677fac7ec12093019a76ad5c6d4eea0eba
Your Name <you@example.com>
2024-10-30 11:37:36 +0000

    updated

--- githook.sh
+++ githook.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export JAVA_HOME=/home/hary/.jdks/xxx
-export PATH=$JAVA_HOME/bin:$PATH
+export JAVA_HOME=/home/hary/opt/corretto-21.0.5
+export M2_HOME=/home/hary/opt/apache-maven-3.9.9
+export PATH=$JAVA_HOME/bin:$M2_HOME/bin/:$PATH
 
 git pull;
 mvn package;
