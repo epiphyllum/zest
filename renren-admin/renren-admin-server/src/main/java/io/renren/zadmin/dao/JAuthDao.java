ddab27fb4ad79615ff24794484a458b81d4a273d
Your Name <you@example.com>
2024-11-18 19:35:08 +0800

    deploy

--- renren-admin/renren-admin-server/src/main/java/io/renren/zadmin/dao/JAuthDao.java
+++ renren-admin/renren-admin-server/src/main/java/io/renren/zadmin/dao/JAuthDao.java
@@ -13,14 +13,15 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface JAuthDao extends BaseDao<JAuthEntity> {
-    @Insert("insert into j_auth(id, agent_id, agent_name, merchant_id, merchant_name, sub_id, sub_name, cardno, logkv, trxcode, trxdir, state, amount, currency, trxtime, mcc, trxaddr, authcode, create_date)\n" +
-            "values (#{id}, #{agentId}, #{agentName}, #{merchantId}, #{merchantName}, #{subId}, #{subName}, #{cardno}, #{logkv}, #{trxcode}, #{trxdir}, #{state}, #{amount}, #{currency}, #{trxtime}, #{mcc}, #{trxaddr}, #{authcode}, #{createDate} )\n" +
+    @Insert("insert into j_auth(id, agent_id, agent_name, merchant_id, merchant_name, sub_id, sub_name, cardno, logkv, trxcode, trxdir, state, amount, settleamount,currency, trxtime, mcc, trxaddr, authcode, create_date)\n" +
+            "values (#{id}, #{agentId}, #{agentName}, #{merchantId}, #{merchantName}, #{subId}, #{subName}, #{cardno}, #{logkv}, #{trxcode}, #{trxdir}, #{state}, #{amount}, #{settleamount}, #{currency}, #{trxtime}, #{mcc}, #{trxaddr}, #{authcode}, #{createDate} )\n" +
             "on duplicate key update\n" +
             "logkv = #{logkv},\n" +
             "trxcode = #{trxcode},\n" +
             "trxdir = #{trxdir},\n" +
             "state = #{state},\n" +
             "amount = #{amount},\n" +
+            "settleamount = #{settleamount},\n" +
             "currency = #{currency},\n" +
             "trxtime = #{trxtime},\n" +
             "mcc = #{mcc},\n" +
