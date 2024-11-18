36edfd65ba3009a5c8577e4cd8698c7b11ec3756
Your Name <you@example.com>
2024-11-18 12:09:52 +0800

    deploy

--- renren-admin/renren-admin-server/src/main/java/io/renren/zmanager/JMerchantManager.java
+++ renren-admin/renren-admin-server/src/main/java/io/renren/zmanager/JMerchantManager.java
@@ -231,12 +231,15 @@ public class JMerchantManager {
 
         // 变为成功, 并开户
         tx.executeWithoutResult(st -> {
-            jMerchantDao.update(null, Wrappers.<JMerchantEntity>lambdaUpdate()
+            int update = jMerchantDao.update(null, Wrappers.<JMerchantEntity>lambdaUpdate()
                     .eq(JMerchantEntity::getId, entity.getId())
                     .eq(JMerchantEntity::getState, oldState)
                     .set(JMerchantEntity::getState, newState)
                     .set(cusid != null, JMerchantEntity::getCusid, cusid)
             );
+            if (update != 1) {
+                throw new RenException("开户失败");
+            }
             // 状态又变化， 且新状态是成功
             if (newState.equals(ZinConstant.MERCHANT_STATE_VERIFIED) || newState.equals(ZinConstant.MERCHANT_STATE_REGISTER)) {
                 this.openVa(entity);
