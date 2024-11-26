3052dbb68a21daaaa09691808a033c35f8f01551
Your Name <you@example.com>
2024-11-26 17:51:00 +0800

    deploy

--- renren-admin/renren-admin-server/src/main/java/io/renren/zin/exchange/CP213.java
+++ renren-admin/renren-admin-server/src/main/java/io/renren/zin/exchange/CP213.java
@@ -3,9 +3,12 @@ package io.renren.zin.exchange;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import io.renren.zadmin.dao.JMoneyDao;
 import io.renren.zadmin.entity.JMoneyEntity;
+import io.renren.zcommon.ZinConstant;
 import io.renren.zin.exchange.dto.TExchangeStateNotify;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // CP213  伞形账户入账
 @Service
@@ -17,6 +20,11 @@ public class CP213 {
     public void handle(TExchangeStateNotify notify) {
         jMoneyDao.update(null, Wrappers.<JMoneyEntity>lambdaUpdate()
                 .eq(JMoneyEntity::getApplyid, notify.getApplyid())
+                .notIn(JMoneyEntity::getState, List.of(
+                        ZinConstant.PAY_APPLY_LG_DJ,
+                        ZinConstant.PAY_APPLY_CLOSE,
+                        ZinConstant.PAY_APPLY_REFUND
+                ))
                 .set(JMoneyEntity::getState, notify.getState())
                 .set(JMoneyEntity::getStateexplain, notify.getStateexplain())
         );
