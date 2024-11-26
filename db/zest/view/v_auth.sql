e4c135d43b3940e72c7d7566909d13bc58842834
Your Name <you@example.com>
2024-11-26 14:27:27 +0800

    updated

--- db/zest/view/v_auth.sql
+++ db/zest/view/v_auth.sql
@@ -1,18 +1,33 @@
+-- sum(trxcode =='Auth' and state == '成功' and  trxdir == '付款’）
+--  - sum(trxcode == 'ReturnOL' and state == '成功' and trxdir == '收款')
+--  - sum(trxcode =='Auth' and state == '成功' &&  trxdir == '收款')
+
+-- 付款：101014
+-- 收款：101013
+
+--
 -- 发卡数量分类汇总
 drop view if exists v_auth;
 create view v_auth as
-select count(1)          as total_card,
-       sum(fee) as fee,
-       sum(merchantfee) as merchantfee,
-       stat_date,
+
+select sum(if(trxcode = 'Auth' && trxdir = '101014', settleamount, 0)) -
+       sum(if(trxcode = 'ReturnOL' && trxdir = '101013', settleamount, 0)) -
+       sum(if(trxcode = 'Auth' && trxdir = '101013', settleamount, 0)) as settleamount,
+       sum(if(trxcode = 'Auth' && trxdir = '101014', 1, 0)) -
+       sum(if(trxcode = 'ReturnOL' && trxdir = '101013', 1, 0)) -
+       sum(if(trxcode = 'Auth' && trxdir = '101013', 1, 0))            as settlecount,
        currency,
        marketproduct,
        merchant_id,
        merchant_name,
        sub_id,
        sub_name,
-       agent_id,
-       agent_name
-from j_card
-where state = '04'
-group by stat_date, agent_id, agent_name, merchant_id, merchant_name, sub_id, sub_name, currency, marketproduct;
+       agent_id
+from j_auth
+
+where state = '00'
+group by agent_id, agent_name, merchant_id, merchant_name, sub_id, sub_name, currency, marketproduct;
+
+
+select *
+from v_auth;
